cd2cfc3846549e686e7b1d85d07bc37a6c9030b7
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-06-04 22:09:51 +0800

    the 2-th version train file

--- train2.py
+++ train2.py
@@ -27,11 +27,11 @@ from IPython import embed
 
 parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
 # Optimization options
-parser.add_argument('--epochs', default=20, type=int, metavar='N',
+parser.add_argument('--epochs', default=2, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('--batch-size', default=16, type=int, metavar='N',
+parser.add_argument('--batch-size', default=2, type=int, metavar='N',
                     help='train batchsize')
 parser.add_argument('--lr', '--learning-rate', default=0.002, type=float,
                     metavar='LR', help='initial learning rate')
@@ -44,9 +44,9 @@ parser.add_argument('--manualSeed', type=int, default=0, help='manual seed')
 parser.add_argument('--gpu', default='0', type=str,
                     help='id(s) for CUDA_VISIBLE_DEVICES')
 #Method options
-parser.add_argument('--n-labeled', type=int, default=50,
+parser.add_argument('--n-labeled', type=int, default=15,
                         help='Number of labeled data')
-parser.add_argument('--train-iteration', type=int, default=375,
+parser.add_argument('--train-iteration', type=int, default=5,
                         help='Number of iteration per epoch')
 parser.add_argument('--out', default='result',
                         help='Directory to output the result')
@@ -184,6 +184,7 @@ def train(labeled_trainloader, unlabeled_trainloader, model, ema_optimizer, crit
 
             try:
                 (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()  ### 问题：这里为什么是(inputs_u, inputs_u2),_？
+                embed()
             except:
                 unlabeled_train_iter = iter(unlabeled_trainloader)
                 # embed()
@@ -205,62 +206,64 @@ def train(labeled_trainloader, unlabeled_trainloader, model, ema_optimizer, crit
                 inputs_u = inputs_u.cuda()
                 inputs_u2 = inputs_u2.cuda()
 
-            with torch.no_grad():
-                # compute guessed labels of unlabel samples
-                # embed()
-                # inputs_u = inputs_u.resize_(64, 3, 512, 512)
-                # outputs_u = model(inputs_u)
-                outputs_u = model(inputs_u)
-                outputs_u2 = model(inputs_u2)
-                # embed()
-                shape1 = outputs_u[0].shape[0]
-                shape2 = outputs_u[0].shape[1]
-                if use_cuda:
-                    p = torch.zeros(shape1, shape2).cuda()
-                else:
-                    p = torch.zeros(shape1, shape2)
-
-                ### 可能后边logits出现的所有的问题都出在这
-                for i in range(len(outputs_u)):
-                    p = p + (torch.softmax(outputs_u[i], dim=1) + torch.softmax(outputs_u2[i], dim=1)) / 2
-                # p = (torch.softmax(outputs_u, dim=1) + torch.softmax(outputs_u2, dim=1)) / 2
-                pt = p ** (1 / args.T)
-                targets_u = pt / pt.sum(dim=1, keepdim=True)
-                targets_u = targets_u.detach()
-
-            # mixup
+            # # mixup
+            # # embed()
+            # ### inputs_x inputs_u inputs_u2进行拼接
+            # all_inputs = torch.cat([inputs_x, inputs_u, inputs_u2], dim=0)
+            # # all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+            #
+            # l = np.random.beta(args.alpha, args.alpha)
+            #
+            # l = max(l, 1 - l)
+            #
+            # idx = torch.randperm(all_inputs.size(0))
+            #
+            # ### input_a input_b是啥？
+            # input_a, input_b = all_inputs, all_inputs[idx]
+            # # target_a, target_b = all_targets, all_targets[idx]
+            #
+            # mixed_input = l * input_a + (1 - l) * input_b
+            # # mixed_target = l * target_a + (1 - l) * target_b
+            #
+            # # interleave labeled and unlabed samples between batches to get correct batchnorm calculation
+            # mixed_input = list(torch.split(mixed_input, batch_size))
+            # print("mixed_input before interleave")
             # embed()
-            ### inputs_x inputs_u inputs_u2进行拼接
-            all_inputs = torch.cat([inputs_x, inputs_u, inputs_u2], dim=0)
-            all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+            # ### 这一个interleave没问题
+            # mixed_input = interleave(mixed_input, batch_size)
 
             l = np.random.beta(args.alpha, args.alpha)
 
             l = max(l, 1 - l)
 
-            idx = torch.randperm(all_inputs.size(0))
+            idx = torch.randperm(6)
 
-            ### input_a input_b是啥？
-            input_a, input_b = all_inputs, all_inputs[idx]
-            target_a, target_b = all_targets, all_targets[idx]
 
-            mixed_input = l * input_a + (1 - l) * input_b
-            mixed_target = l * target_a + (1 - l) * target_b
+            ### 单个mixed_input
+            mixed_input1 = mixed_input_generator(inputs_x, 8, inputs_u, inputs_u2, batch_size)
+            mixed_input2 = mixed_input_generator(inputs_x, 4, inputs_u, inputs_u2, batch_size)
+            mixed_input3 = mixed_input_generator(inputs_x, 2, inputs_u, inputs_u2, batch_size)
+            mixed_input = mixed_input_generator(inputs_x, 0, inputs_u, inputs_u2, batch_size)
 
-            # interleave labeled and unlabed samples between batches to get correct batchnorm calculation
-            mixed_input = list(torch.split(mixed_input, batch_size))
-            ### 这一个interleave没问题
-            mixed_input = interleave(mixed_input, batch_size)
+
+            ### 单个mixed_target
+            new_mixed_target1 = mixed_target_generator(model, 0, inputs_u, inputs_u2, targets_x, l, idx)
+            new_mixed_target2 = mixed_target_generator(model, 1, inputs_u, inputs_u2, targets_x, l, idx)
+            new_mixed_target3 = mixed_target_generator(model, 2, inputs_u, inputs_u2, targets_x, l, idx)
+            new_mixed_target = mixed_target_generator(model, 3, inputs_u, inputs_u2, targets_x, l, idx)
+
+
 
             for nlr in range(len(optimizer.param_groups)):
                 optimizer.param_groups[nlr]['lr'] = cosine_anneal_schedule(epoch, args.epochs, lr[nlr])
 
             ###### input1
             optimizer.zero_grad()
-            ### get mixed_input1
-            mixed_input1 = jigsaw_generator(mixed_input, 8)
+            print("mixed_input after interleave")
+
+            ### input1
             logits = get_logits(0, model, mixed_input1)
-            loss1 = get_loss(logits, criterion, mixed_target, batch_size, batch_idx, epoch)
+            loss1 = get_loss(logits, criterion, new_mixed_target1, batch_size, batch_idx, epoch)
             # record loss
             losses1.update(loss1.item(), inputs_x.size(0))
             loss1.backward()
@@ -270,9 +273,9 @@ def train(labeled_trainloader, unlabeled_trainloader, model, ema_optimizer, crit
             ###### input2
             optimizer.zero_grad()
             ### get mixed_input2
-            mixed_input2 = jigsaw_generator(mixed_input, 4)
+            # mixed_input2 = jigsaw_generator(mixed_input, 4)
             logits = get_logits(1, model, mixed_input2)
-            loss2 = get_loss(logits, criterion, mixed_target, batch_size, batch_idx, epoch)
+            loss2 = get_loss(logits, criterion, new_mixed_target2, batch_size, batch_idx, epoch)
             # record loss
             losses2.update(loss2.item(), inputs_x.size(0))
             loss2.backward()
@@ -282,9 +285,9 @@ def train(labeled_trainloader, unlabeled_trainloader, model, ema_optimizer, crit
             ###### input3
             optimizer.zero_grad()
             ### get mixed_input3
-            mixed_input3 = jigsaw_generator(mixed_input, 2)
+            # mixed_input3 = jigsaw_generator(mixed_input, 2)
             logits = get_logits(2, model, mixed_input3)
-            loss3 = get_loss(logits, criterion, mixed_target, batch_size, batch_idx, epoch)
+            loss3 = get_loss(logits, criterion, new_mixed_target3, batch_size, batch_idx, epoch)
             # record loss
             losses3.update(loss3.item(), inputs_x.size(0))
             loss3.backward()
@@ -294,7 +297,7 @@ def train(labeled_trainloader, unlabeled_trainloader, model, ema_optimizer, crit
             ###### total input
             optimizer.zero_grad()
             logits = get_logits(3, model, mixed_input)
-            loss = get_loss(logits, criterion, mixed_target, batch_size, batch_idx, epoch)
+            loss = get_loss(logits, criterion, new_mixed_target, batch_size, batch_idx, epoch)
             # record loss
             losses.update(loss.item(), inputs_x.size(0))
             loss.backward()
@@ -380,26 +383,105 @@ def validate(valloader, model, criterion, use_cuda, mode):
         bar.finish()
     return (losses.avg, top1.avg)
 
-### get_mixed_input
+def mixed_input_generator(inputs_x,n,inputs_u,inputs_u2,batch_size):
+    ### 先进行拼图处理
+    if n > 0:# n < 0表示total_input,不进行拼图处理
+        inputs_x = jigsaw_generator(inputs_x, n)
+        inputs_u = jigsaw_generator(inputs_x, n)
+        inputs_u2 = jigsaw_generator(inputs_x, n)
+
+    ### inputs_x inputs_u inputs_u2进行拼接
+    all_inputs = torch.cat([inputs_x, inputs_u, inputs_u2], dim=0)
+    # all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+
+    l = np.random.beta(args.alpha, args.alpha)
+
+    l = max(l, 1 - l)
+
+    idx = torch.randperm(all_inputs.size(0))
+    print("idx in mixed_input_generator:",idx)
+
+
+    ### input_a input_b是啥？
+    input_a, input_b = all_inputs, all_inputs[idx]
+    # target_a, target_b = all_targets, all_targets[idx]
+
+    mixed_input = l * input_a + (1 - l) * input_b
+    # mixed_target = l * target_a + (1 - l) * target_b
+
+    # interleave labeled and unlabed samples between batches to get correct batchnorm calculation
+    mixed_input = list(torch.split(mixed_input, batch_size))
+    print("mixed_input before interleave")
+    embed()
+    ### 这一个interleave没问题
+    mixed_input = interleave(mixed_input, batch_size)
+
+    return mixed_input
+
+def mixed_target_generator(model,outputs_id,inputs_u,inputs_u2,targets_x,l,idx):
+    with torch.no_grad():
+        # compute guessed labels of unlabel samples
+        # embed()
+        # inputs_u = inputs_u.resize_(64, 3, 512, 512)
+        # outputs_u = model(inputs_u)
+
+        ### 不同的mixed_input对应不同的outputs_u
+        outputs_u = model(inputs_u)[outputs_id]
+        outputs_u2 = model(inputs_u2)[outputs_id]
+
+        p = (torch.softmax(outputs_u, dim=1) + torch.softmax(outputs_u2, dim=1)) / 2
+        # p = (torch.softmax(outputs_u, dim=1) + torch.softmax(outputs_u2, dim=1)) / 2
+        pt = p ** (1 / args.T)
+        targets_u = pt / pt.sum(dim=1, keepdim=True)
+        targets_u = targets_u.detach()
+
+    all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+
+    target_a, target_b = all_targets, all_targets[idx]
+
+    mixed_target = l * target_a + (1 - l) * target_b
+
+    return mixed_target
+
+### 这是干啥的？
 def jigsaw_generator(images, n):
-    result = []
-    for image in images:
-        l = []
-        for a in range(n):
-            for b in range(n):
-                l.append([a, b])
-        block_size = 448 // n
-        rounds = n ** 2
-        random.shuffle(l)
-        jigsaws = image.clone()
-        for i in range(rounds):
-            x, y = l[i]
-            temp = jigsaws[..., 0:block_size, 0:block_size].clone()
-            jigsaws[..., 0:block_size, 0:block_size] = jigsaws[..., x * block_size:(x + 1) * block_size,
-                                                       y * block_size:(y + 1) * block_size].clone()
-            jigsaws[..., x * block_size:(x + 1) * block_size, y * block_size:(y + 1) * block_size] = temp
-        result.append(jigsaws)
-    return result
+    l = []
+    for a in range(n):
+        for b in range(n):
+            l.append([a, b])
+    block_size = 448 // n
+    rounds = n ** 2
+    random.shuffle(l)
+    jigsaws = images.clone()
+    for i in range(rounds):
+        x, y = l[i]
+        temp = jigsaws[..., 0:block_size, 0:block_size].clone()
+        jigsaws[..., 0:block_size, 0:block_size] = jigsaws[..., x * block_size:(x + 1) * block_size,
+                                                y * block_size:(y + 1) * block_size].clone()
+        jigsaws[..., x * block_size:(x + 1) * block_size, y * block_size:(y + 1) * block_size] = temp
+
+    return jigsaws
+
+### get_mixed_input
+# def jigsaw_generator(images, n):
+#     result = []
+#     for image in images:
+#         l = []
+#         for a in range(n):
+#             for b in range(n):
+#                 l.append([a, b])
+#         block_size = 448 // n
+#         rounds = n ** 2
+#         random.shuffle(l)
+#         jigsaws = image.clone()
+#         for i in range(rounds):
+#             x, y = l[i]
+#             temp = jigsaws[..., 0:block_size, 0:block_size].clone()
+#             jigsaws[..., 0:block_size, 0:block_size] = jigsaws[..., x * block_size:(x + 1) * block_size,
+#                                                        y * block_size:(y + 1) * block_size].clone()
+#             jigsaws[..., x * block_size:(x + 1) * block_size, y * block_size:(y + 1) * block_size] = temp
+#         result.append(jigsaws)
+#     return result
 
 
 
@@ -417,19 +499,6 @@ def get_logits(input_id,model,mixed_input):
 
 def get_loss(logits,criterion,mixed_target,batch_size,batch_idx,epoch):
 
-    # logits = []
-    # # embed()
-    # logits.append(model(mixed_input[0])[3])
-    # ### model(mixed_input[0])[3]相当于_, _, _, logit = model(mixed_input[0])
-    # ### targets_u
-    # for input in mixed_input[1:]:
-    #     _, _, _, logit = model(input)
-    #     logits.append(logit)  ### 特征提取
-
-    ### 此时logits[i].shape [2,200]
-    # put interleaved samples back
-    ### 问题出在这个interleave上
-    # embed()
     logits = interleave(logits, batch_size)
     # embed()
     ### 原来的代码
@@ -513,21 +582,6 @@ def interleave(xy, batch):
     # embed()
     return [torch.cat(v, dim=0) for v in xy]
 
-def interleave_(xy, batch):
-    nu = len(xy) - 1
-    offsets = interleave_offsets(batch, nu)
-    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
-    for i in range(1, nu + 1):
-        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
-    # embed()
-    total_tensors = []
-    for v in xy:
-        tensors = []
-        for vv in v:
-            if len(vv) > 0:
-                tensors.append(vv[0])# vv[0]是[2,200]所以cat之后就是[4,200]
-        total_tensors.append(tensors)
-    return [torch.cat(v, dim=0) for v in total_tensors]
 
 if __name__ == '__main__':
     main()
